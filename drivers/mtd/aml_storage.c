ff7c19fff787d4c2aa41f49ab5d2c57be86261b7
Vitaliy Tomin <highwaystar.ru@gmail.com>
2025-03-07 23:36:52 +0800

    fixup! drivers: mtd: fix control reaches end of non-void function

--- drivers/mtd/aml_storage.c
+++ drivers/mtd/aml_storage.c
@@ -496,11 +496,11 @@ static int mtd_store_count(void)
 {
 #if defined(CONFIG_CMD_MTDPARTS)
 	if (mtdparts_init())
-#endif
 #ifdef CONFIG_AML_MTDPART
 	return get_aml_mtdpart_count();
 	else
 	return 0;
+#endif
 #else
 	return 0;
 #endif
